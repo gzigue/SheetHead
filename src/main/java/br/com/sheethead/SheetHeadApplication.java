b2643e9964e844f8ac7b4bdfe9b1c4867390ef55
Gabriel <gzigue@cg-001530.cwinet.local>
2018-12-14 15:45:48 -0200

    Mais um projeto

---
+++ src/main/java/br/com/sheethead/SheetHeadApplication.java
@@ -0,0 +1,14 @@
+package br.com.sheethead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetHeadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheetHeadApplication.class, args);
+	}
+
+}
+
